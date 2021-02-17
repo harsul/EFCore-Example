b97c938829a6e46b503855bc99aac6be4f6a9277
Harun Suljagic <harun.suljagic@hotmail.com>
2021-02-17 21:40:06 +0300

    Add project files.

---
+++ EFCore - Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFCore___Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
